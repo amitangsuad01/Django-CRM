eb53775337b844dc554f619b06f1c13d23d1b17a
Amitangsu <62561642+amitangsuad01@users.noreply.github.com>
2023-04-19 10:42:41 +0530

    Home Page Added

--- website/views.py
+++ website/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
+def home(req):
+    return render(req, 'home.html', {})
+
+def login_user(req):
+    pass
+
+def logout_user(req):
+    pass
+
